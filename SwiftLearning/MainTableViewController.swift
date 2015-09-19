fd26b3e2e76eaa3083f592a9878f3dd8a3aa5bb3
Mohanad Almiski <malmiski@live.com>
2015-09-19 16:35:22 -0400

    Basic Table that goes to a view controller

---
+++ SwiftLearning/MainTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MainTableViewController.swift
+//  SwiftLearning
+//
+//  Created by Basement on 9/19/15.
+//  Copyright (c) 2015 Mohanad. All rights reserved.
+//
+
+import UIKit
+class MainTableViewController : UITableViewController{
+
+}
